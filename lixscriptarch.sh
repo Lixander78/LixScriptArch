05ea9431be197bb3542764ffc3de2e5a718b1239
Lixander78 <66738958+Lixander78@users.noreply.github.com>
2021-11-11 15:58:50 +0200

    Update lixscriptarch.sh

--- lixscriptarch.sh
+++ lixscriptarch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/ArchTitus/1-setup.sh
-    source /mnt/root/ArchTitus/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
-    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+    arch-chroot /mnt /root/LixScriptArch/1-setup.sh
+    source /mnt/root/LixScriptArch/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/LixScriptArch/2-user.sh
+    arch-chroot /mnt /root/LixScriptArch/3-post-setup.sh
